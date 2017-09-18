34c5ad0edd1842fdcf540a86ed25d09d33c3f5e9
Grzegorz Pawlak <kalejdoskopwroclaw@gmail.com>
2017-09-18 11:44:39 +0200

    add

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // scripts.js
-var a = prompt('Wartość A:','6');
+var a = prompt('Wartość A:', '6');
 var h = prompt('Wartość B:', '10');
 var value = getTriangleArea(a, h);
 console.log('Wynik działania ' + a + ' and ' + h + ' is equal to ' + value);
@@ -7,14 +7,14 @@ console.log('Wynik działania ' + a + ' and ' + h + ' is equal to ' + value);
 function getTriangleArea(a, h) {
 	if (a <= 0 || h <= 0) {
 		return 'nieprawidłowe dane';
-	} else if (a > 0 && h > 0) {
-		return a * h / 2
+	} else {
+		return a * h / 2;
 	}
 };
 
-console.log( getTriangleArea(4,7));
-console.log( getTriangleArea(10,6));
-console.log( getTriangleArea(-10,10));
+console.log(getTriangleArea(4,7));
+console.log(getTriangleArea(10,6));
+console.log(getTriangleArea(-10,10));
 
 var triangle1Area = getTriangleArea(10, 15);
 var triangle2Area = getTriangleArea(13, -12);
@@ -25,4 +25,4 @@ console.log(triangle2Area);
 console.log(triangle3Area);
 
 
-document.getElementById("prompt").innerHTML = 'Twój wynik to:' + value
+document.getElementById("prompt").innerHTML = 'Twój wynik to:' + value;
